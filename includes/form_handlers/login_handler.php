ae3bcc5716dc51ef1e19c3c61102e2a8aa1848ca
kohei <k_kojima0407@yahoo.co.jp>
2020-04-05 16:20:08 +0900

    閉鎖されたアカウントを再開させる

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -22,6 +22,13 @@ if(isset($_POST['login_button'])) {
     $row = mysqli_fetch_array($check_database_query); //連想配列で取得する
     $username = $row['username'];
 
+    // ユーザーがclosedになっていた場合、再度ログインしたらユーザーを元に戻す
+    $user_closed_query = mysqli_query($con,"SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+
+    if(mysqli_num_rows($user_closed_query) == 1) {
+      $reopen_account = mysqli_query($con,"UPDATE users SET user_closed='no' WHERE email='$email'");
+    }
+
     $_SESSION['username'] = $username;
 
     header("Location:index.php");
