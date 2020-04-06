97bdce02f2a645e3c205b8c836f6445f8ce3198b
kohei <k_kojima0407@yahoo.co.jp>
2020-04-06 23:14:54 +0900

    ユーザーパネルへ名前、投稿数、いいね数の表示

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <?php
 include("includes/header.php");
-
 //ページ更新でセッションを破棄する
 // session_destroy();
-
 ?>
 
-<div class="user_details column">
-  <a href="#"><img src="<?php echo $user['profile_pic'];?>" alt=""></a>
-</div>
+  <div class="user_details column">
+      <a href="#"><img src="<?php echo $user['profile_pic'];?>" alt=""></a>
 
+      <div class="user_details_left_right">
+        <a href="#">
+        <?php
+        echo $user['first_name'] . " " . $user['last_name'];
+        ?>
+        </a>
+        <?php
+        echo "POSTS:" . $user['num_posts']. "<br>";
+        echo "LIKES:" . $user['num_likes'];
+        ?>
+      </div>
 
-</div><!-- wrapper -->
+    </div>
+
+  </div><!-- wrapper -->
 
 </body>
 </html>
