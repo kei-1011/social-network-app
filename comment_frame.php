754f17238fe92a8272ec2c6492c41ce7ac9537ca
kohei <k_kojima0407@yahoo.co.jp>
2020-05-15 19:36:27 +0900

    コメントの読み込み

--- comment_frame.php
+++ comment_frame.php
@@ -128,10 +128,17 @@ if($count != 0) {
       }
     }
     $user_obj = new User($con,$posted_by);
+    ?>
+      <div class="comment_section">
+      <!-- target _parentで親要素にリンクさせる -->
+        <a href="<?php echo $posted_by;?>" target="_parent"><img src="<?php echo $user_obj->getProfilePic();?>" title="<?php echo $posted_by;?>" style="float:left;" height="30"></a>
+        <a href="<?php echo $posted_by;?>" target="_parent"><b> <?php echo $user_obj->getFirstAndLastName();?> </b></a>
+        &nbsp;&nbsp;&nbsp;&nbsp;<?php echo $time_message . "<br>" . $comment_body; ?>
+        <hr>
+      </div>
+    <?php
   }
 }
 ?>
-
-<div class="comment_section">
-  <a href="<?php echo $posted_by;?>">aaaaa</a>
-</div>
+</body>
+</html>
