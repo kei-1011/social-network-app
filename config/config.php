1030a53acd0b6842ff52c69d03b67e06b4d04125
kohei <k_kojima0407@yahoo.co.jp>
2020-05-16 16:05:58 +0900

    いいねボタンを押してもUPDATEされない問題を修正

--- config/config.php
+++ config/config.php
@@ -2,6 +2,8 @@
 ob_start(); //出力のバッファリングを有効にする
 session_start();
 
+// ini_set('display_errors', "On");
+
 $timezone = date_default_timezone_set("Asia/Tokyo");
 
 $con = mysqli_connect('localhost', 'root', 'root', 'social');
