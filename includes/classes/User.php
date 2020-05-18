347394332ba2ee8d2e3cc0aa71d808fb5c37dc30
kohei <k_kojima0407@yahoo.co.jp>
2020-05-18 09:46:00 +0900

    follor button style

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -62,7 +62,7 @@ class User {
     }
   }
 
-  public function didReceiveRequest($username_to_check) {
+  public function didReceiveRequest($user_to) {
     $user_from = $this->user['username'];
     $check_request_query = mysqli_query($this->con,"SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
     if(mysqli_num_rows($check_request_query ) > 0) {
@@ -73,7 +73,7 @@ class User {
   }
 
   public function didSendRequest($user_from) {
-    $user_from = $this->user['username'];
+    $user_to = $this->user['username'];
     $check_request_query = mysqli_query($this->con,"SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
     if(mysqli_num_rows($check_request_query ) > 0) {
       return true;
