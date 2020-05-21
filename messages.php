a9a46d3d2e9bbd8b766e8288e9e9dec70bcba791
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-05-21 09:10:25 +0900

    create message class

---
+++ messages.php
@@ -0,0 +1,15 @@
+<?php
+include("includes/header.php");
+include("includes/classes/Message.php");
+
+$message_obj = new Message($con, $userLoggedIn);
+
+if(isset($_GET['u'])) {
+  $user_to = $_GET['u'];
+} else {
+  $user_to = $message_obj->getMostRecentUser();
+
+  if ($user_to == false) {
+    $user_to = 'new';
+  }
+}
